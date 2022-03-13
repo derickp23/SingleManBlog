236b8d6d3d3c77482962ea9c9a543e0226414dc5
derickp23 <91038353+derickp23@users.noreply.github.com>
2022-03-12 19:54:09 -0500

    updating code

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Javascript for button to hide quotes */
+function hidequotes() {
+    var x = document.getElementById("Relationshipquotes");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";}}
+
+
+ function dropquotes() {
+    var x = document.getElementById("InspirationalQuote");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";}}      
+      
+  
+
+
+
+        
+
+       
